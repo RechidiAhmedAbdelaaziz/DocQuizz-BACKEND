64e149b1cf885366d9cc6b4e2e602ffc2dcdc4b4
Rechidi Ahmed <aa.rechidi@esi-sba.dz>
2024-09-04 10:00:59 +0100

    chore: Add mailer module
    This commit adds the `mailer` module to the project. It includes the `MailerController` and `MailerService` classes, which handle email functionality using the `nodemailer` package. This addition allows for the integration of email sending capabilities into the application.

---
+++ libs/common/src/module/mailer/mailer.service.ts
@@ -0,0 +1,59 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { Auth, google } from 'googleapis';
+import { createTransport, Transporter } from 'nodemailer';
+
+@Injectable()
+export class MailerService {
+
+    private gooleAuthClient: Auth.OAuth2Client
+    private transport: Transporter
+
+
+
+    constructor() {
+        const clientId = process.env.GOOGLE_MAILER_CLIENT_ID
+        const clientSecret = process.env.GOOGLE_MAILER_CLIENT_SECRET
+        const redirectUri = process.env.GOOGLE_MAILER_REDIRECT_URI
+        const refreshToken = process.env.GOOGLE_MAILER_REFRESH_TOKEN
+
+        this.gooleAuthClient = new google.auth.OAuth2(clientId, clientSecret, redirectUri)
+        this.gooleAuthClient.setCredentials({ refresh_token: refreshToken })
+
+        this.transport = createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: process.env.GOOGLE_MAILER_USER,
+                clientId: process.env.GOOGLE_MAILER_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_MAILER_CLIENT_SECRET,
+                refreshToken: process.env.GOOGLE_MAILER_REFRESH_TOKEN,
+            }
+        })
+
+    }
+
+
+
+
+    sendMail = async (details: {
+        to: string, subject?: string, text?: string
+    }) => {
+        //set access token in transporter and send email
+        try {
+            const accessToken = (await this.gooleAuthClient.getAccessToken()).token
+            this.transport.set('oauth2_provision_cb', (user, renew, callback) => {
+                callback(null, accessToken);
+            })
+
+            return await this.transport.sendMail({
+                from: 'DocQuizz <' + process.env.GOOGLE_MAILER_USER + '>',
+                to: details.to,
+                subject: details.subject,
+                text: details.text
+            })
+        }
+        catch (err) {
+            throw new HttpException('Error in sending email', 500, { cause: err })
+        }
+    }
+}
