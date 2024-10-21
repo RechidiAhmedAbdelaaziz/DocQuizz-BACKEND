f7042f8b7aa3fc89690737cf3f6c986dc7d415e0
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2024-10-21 11:39:20 +0100

    Refactor file paths in .gitignore and playlist.service.ts

--- apps/quiz-app/src/module/playlist/playlist.service.ts
+++ apps/quiz-app/src/module/playlist/playlist.service.ts
@@ -183,7 +183,7 @@ export class PlaylistService {
             .populate({
                 path: 'questions',
                 populate: [
-                    { path: 'exam' },
+                    // { path: 'exams' },
                     { path: 'course' },
                     { path: 'sources.source' }
                 ]
