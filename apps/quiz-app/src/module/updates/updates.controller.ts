667aa571724dc0d234bd803c7b167133aa8c27b4
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2024-10-27 14:31:22 +0100

    Refactor updates model to add newline at end of file

--- apps/quiz-app/src/module/updates/updates.controller.ts
+++ apps/quiz-app/src/module/updates/updates.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UpdatesService } from './updates.service';
+import { HttpAuthGuard } from '@app/common';
 
 @Controller('updates')
 export class UpdatesController {
   constructor(private readonly updatesService: UpdatesService) { }
 
+  @UseGuards(HttpAuthGuard)
   @Get() //* UPDATES | Get ~ {{host}}/updates
   async getUpdates() {
     return await this.updatesService.getUpdates()
--- libs/common/src/models/updates.model.ts
+++ libs/common/src/models/updates.model.ts
@@ -19,3 +19,4 @@ export class Updates extends AbstractSchema {
     date: Date
 
 }
+
