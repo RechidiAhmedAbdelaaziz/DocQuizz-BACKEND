f0efcc29c9e27100de1b5b65eb2f0170af33a982
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2024-10-28 22:21:49 +0100

    Refactor exam-record.module.ts to import and use DatabaseModule for ExamRecord model

--- apps/admin/src/module/exam-record/exam-record.module.ts
+++ apps/admin/src/module/exam-record/exam-record.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ExamRecordService } from './exam-record.service';
 import { ExamRecordController } from './exam-record.controller';
+import { DatabaseModule } from '@app/common';
+import { ExamRecord } from '@app/common/models';
 
 @Module({
+  imports: [
+    DatabaseModule.forFeature([{ model: ExamRecord }]),
+  ],
   controllers: [ExamRecordController],
   providers: [ExamRecordService],
   exports: [ExamRecordService]
